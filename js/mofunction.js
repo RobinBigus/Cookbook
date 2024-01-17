bef02a27f0931149c66d0b6b09b3bf4543cf2396
Moody458 <morentrop@gmail.com>
2024-01-17 14:57:07 +0100

    second button function

--- js/mofunction.js
+++ js/mofunction.js
@@ -1,13 +1,30 @@
+const recipeDisplayClass = document.querySelector('.recipeDisplay')
 const enteRezept = document.querySelector('#duckRecipe');
-let isClicked = true;
+const steakRezept = document.querySelector('#steakSaladRecipe');
+
+let isClickedDuck = true;
+let isClickedSteak = true;
 
 let showDuck = function(){
-    if(isClicked){
+    if(isClickedDuck){
         enteRezept.style.display = 'flex';
-        isClicked = false;
+        isClickedDuck = false;
     }
     else{
         enteRezept.style.display = 'none';
-        isClicked = true;
+        recipeDisplayClass.style.display = 'none';
+        isClickedDuck = true;
     }
-}
+};
+
+let showSteak = function(){
+    if(isClickedSteak){
+        steakRezept.style.display = 'flex';
+        isClickedSteak = false;
+    }
+    else{
+        steakRezept.style.display = 'none';
+        recipeDisplayClass.style.display = 'none';
+        isClickedSteak = true;
+    }
+};
