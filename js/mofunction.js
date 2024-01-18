a28926d2eb2543680f5de37a62b208596d5ab338
Moody458 <morentrop@gmail.com>
2024-01-18 11:22:32 +0100

    added functions for dinner no veg

--- js/mofunction.js
+++ js/mofunction.js
@@ -1,30 +1,104 @@
-const recipeDisplayClass = document.querySelector('.recipeDisplay')
 const enteRezept = document.querySelector('#duckRecipe');
 const steakRezept = document.querySelector('#steakSaladRecipe');
+const chickenRezept = document.querySelector('#crispyChickRecipe');
+const lachsRezept = document.querySelector('#salmonRecipe');
+const cabonaraRezept = document.querySelector('#cabonaraRecipe');
+const wustRezept = document.querySelector('#sausageRecipe');
 
-let isClickedDuck = true;
-let isClickedSteak = true;
+let isClicked = true;
 
 let showDuck = function(){
-    if(isClickedDuck){
+    if(isClicked){
         enteRezept.style.display = 'flex';
-        isClickedDuck = false;
+        steakRezept.style.display = 'none';
+        chickenRezept.style.display = 'none';
+        lachsRezept.style.display = 'none';
+        cabonaraRezept.style.display = 'none';
+        wustRezept.style.display = 'none';
+        isClicked = false;
     }
     else{
         enteRezept.style.display = 'none';
-        recipeDisplayClass.style.display = 'none';
-        isClickedDuck = true;
+        isClicked = true;
     }
 };
 
 let showSteak = function(){
-    if(isClickedSteak){
+    if(isClicked){
+        enteRezept.style.display = 'none';
         steakRezept.style.display = 'flex';
-        isClickedSteak = false;
+        chickenRezept.style.display = 'none';
+        lachsRezept.style.display = 'none';
+        cabonaraRezept.style.display = 'none';
+        wustRezept.style.display = 'none';
+        isClicked = false;
     }
     else{
         steakRezept.style.display = 'none';
-        recipeDisplayClass.style.display = 'none';
-        isClickedSteak = true;
+        isClicked = true;
+    }
+};
+
+let showChicken = function(){
+    if(isClicked){
+        enteRezept.style.display = 'none';
+        steakRezept.style.display = 'none';
+        chickenRezept.style.display = 'flex';
+        lachsRezept.style.display = 'none';
+        cabonaraRezept.style.display = 'none';
+        wustRezept.style.display = 'none';
+        isClicked = false;
+    }
+    else{
+        chickenRezept.style.display = 'none';
+        isClicked = true;
+    }
+};
+
+let showSalmon = function(){
+    if(isClicked){
+        enteRezept.style.display = 'none';
+        steakRezept.style.display = 'none';
+        chickenRezept.style.display = 'none';
+        lachsRezept.style.display = 'flex';
+        cabonaraRezept.style.display = 'none';
+        wustRezept.style.display = 'none';
+        isClicked = false;
+    }
+    else{
+        lachsRezept.style.display = 'none';
+        isClicked = true;
+    }
+};
+
+let showCabonara = function(){
+    if(isClicked){
+        enteRezept.style.display = 'none';
+        steakRezept.style.display = 'none';
+        chickenRezept.style.display = 'none';
+        lachsRezept.style.display = 'none';
+        cabonaraRezept.style.display = 'flex';
+        wustRezept.style.display = 'none';
+        isClicked = false;
+    }
+    else{
+        cabonaraRezept.style.display = 'none';
+        isClicked = true;
+    }
+};
+
+let showSausage = function(){
+    if(isClicked){
+        enteRezept.style.display = 'none';
+        steakRezept.style.display = 'none';
+        chickenRezept.style.display = 'none';
+        lachsRezept.style.display = 'none';
+        cabonaraRezept.style.display = 'none';
+        wustRezept.style.display = 'flex';
+        isClicked = false;
+    }
+    else{
+        wustRezept.style.display = 'none';
+        isClicked = true;
     }
 };
